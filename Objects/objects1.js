b383f12043f8f5e69508cca3dfc179b2a4a93301
BIT Student <classroom@bgit.rs>
2020-02-26 18:01:56 +0100

    Second commit

--- Objects/objects1.js
+++ Objects/objects1.js
@@ -36,7 +36,7 @@ var movie2 = {
     actors: ["Orson Welles", "Joseph Cotten"],
     director: "Orson Welles",
     genre: "Drama",
-    imdb: "greatest movie of all time"
+    popularity: Infinity
 }
 
 console.log(movie2.actors[1]);
@@ -46,26 +46,34 @@ console.log(movie2.actors[1]);
 // Add a method that prints out the project's repository,  a method that checks if the project is written in JavaScript 
 // as well as a method that checks if the project is in development or not.
 
-
-function project(pn, ds, pl, gr, bs) {
-
-    var pr = {
-        projectName: pn,
-        description: ds,
-        programmingLanguage: pl,
-        gitRepository: gr,
-        booleanStatus: bs,
-
+var f = function (description, programmingLanguage, gitRepository, inDevelopment) {
+    var project = {
+        description: description,
+        programmingLanguage: programmingLanguage,
+        gitRepository: gitRepository,
+        inDevelopment: inDevelopment,
+        printRepository: function () {
+            console.log(this.gitRepository + "\n|-folder1" + "\n|-folder2" + "\n|_Main.exe");
+        },
+        isWrittenInJavascript: function () {
+            if (this.programmingLanguage === 'Javascript') {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        isInDevelopment: function () {
+            return this.inDevelopment;
+        }
     }
-    return pr;
+    return project;
 }
 
-
-
-
-var pro = project("objects", "learning", "js", "objects-git", "true");
-
-console.log(pro);
+var project = f('README.txt', 'C-esharp', 'https://github.com/Username/repository.git', false)
+console.log(project);
+project.printRepository();
+console.log(project.isWrittenInJavascript());
+console.log(project.isInDevelopment());
 
 //4. Write a function that creates an object that represents a culinary recipe. 
 // Each recipe is described by: name, type of cuisine, complexity (value from 1 to 5), list of ingredients, preparing time, preparing instruction. 
@@ -73,3 +81,118 @@ console.log(pro);
 // Add a method that checks if a meal can be prepared for 15 minutes. 
 // Add a method that changes the type of cuisine to the given value. 
 // Add a method that delete a given ingredient from the list of ingredients.
+
+var f = function (name, typeOfCuisine, Complexity, listOfIngredients, preparingTime, preparingInstruction) {
+    var recipe = {
+        name: name,
+        typeOfCuisine: typeOfCuisine,
+        Complexity: Complexity,
+        listOfIngredients: listOfIngredients,
+        preparingTime: preparingTime,
+        preparingInstruction: preparingInstruction,
+        printIngredients: function () {
+            var res = '';
+
+            for (var i = 0; i < this.listOfIngredients.length; i++) {
+                res += this.listOfIngredients[i] + "\n";
+            }
+            console.log(res);
+        },
+        under15min: function () {
+            if (this.preparingTime <= 15) {
+                console.log("Can be prepared in less than 15 min");
+            } else {
+                console.log("No can do!");
+            }
+        },
+        setTypeOfCuisine: function (typeOfCuisine) {
+            this.typeOfCuisine = typeOfCuisine;
+        },
+        removeIngredient: function (ingredientToRemove) {
+            var filteredIngredients = [];
+
+            for (var i = 0; i < listOfIngredients.length; i++) {
+                if (this.listOfIngredients !== ingredientToRemove) {
+                    filteredIngredients[filteredIngredients.length] = this.listOfIngredients[i];
+                }
+            }
+            this.listOfIngredients = filteredIngredients;
+        },
+        print: function () {
+            console.log(
+                "Name            => " + this.name + "\n" +
+                "Cuisine         => " + this.typeOfCuisine + "\n" +
+                "Ingredients     => " + this.listOfIngredients + "\n" +
+                "Time to prepare => " + this.preparingTime + "\n"
+            )
+        }
+    }
+    return recipe;
+}
+
+var recipe = f("Carbonara", "it", 3, ['pasta', 'parmigiano', 'eggs'], 10, 'improvise');
+console.log(recipe);
+console.log(recipe.printIngredients());
+console.log(recipe.under15min());
+console.log(recipe.setTypeOfCuisine("pastiche"));
+recipe.removeIngredient('eggs');
+console.log(recipe.printIngredients());
+recipe.print();
+
+// 
+
+var createProject = function (description, progLang, gitRep, isInDevel) {
+    var project = {
+        description: description,
+        progLang: progLang,
+        gitRep: gitRep,
+        isInDevel: isInDevel,
+        printRep: function () {
+            console.log(this.gitRep);
+        },
+        isWrittenInJavascript: function () {
+            console.log(this.progLang === "js");
+
+        },
+        isInDevelopment: function () {
+            console.log(this.isInDevel);
+
+        }
+    }
+    return project;
+}
+
+var project = createProject("description", "css", "repository", true);
+project.printRep();
+project.isWrittenInJavascript();
+project.isInDevelopment();
+
+//--------------------------------------------------------------------
+
+function Project(description, programmingLanguage, gitRepository, inDevelopment) {
+    this.description = description;
+    this.programmingLanguage = programmingLanguage;
+    this.gitRepository = gitRepository;
+    this.inDevelopment = inDevelopment;
+    this.printRepository = function () {
+        console.log(this.gitRepository + "\n|-folder1" + "\n|-folder2" + "\n|_Main.exe");
+    };
+    this.isWrittenInJavascript = function () {
+        if (this.programmingLanguage === 'Javascript') {
+            return true;
+        } else {
+            return false;
+        }
+    };
+    this.isInDevelopment = function () {
+        return this.inDevelopment;
+    };
+}
+
+var project = new Project('README.txt', 'C-esharp', 'https://github.com/Username/repository.git', false)
+console.log(project);
+project.printRepository();
+console.log(project.isWrittenInJavascript());
+console.log(project.isInDevelopment());
+
+
